dac58a98044d77ece0e0a3193d195c954729f65b
virusman1 <xirb_01@hotmail.co.th>
2019-01-22 01:39:28 +0700

    Update DemoApplication.java

--- src/main/java/com/se/team00/demo/DemoApplication.java
+++ src/main/java/com/se/team00/demo/DemoApplication.java
@@ -1,7 +1,15 @@
 package com.se.team00.demo;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import com.se.team00.demo.entity.*;
+import com.se.team00.demo.repository.*;
+import java.util.stream.Stream;
+
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,5 +18,20 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Bean
+    ApplicationRunner init(StudentRepository studentRepository) {
+		
+		return args -> {
+				Stream.of("B5720651").forEach(studentId -> {
+				Student student = new Student();
+				student.setStudentId(studentId);
+				student.setLastName("sit");
+				student.setFirstName("siri");
+				studentRepository.save(student);
+		});
+		studentRepository.findAll().forEach(System.out::println);
+	};
+			}
+	
 }
 
