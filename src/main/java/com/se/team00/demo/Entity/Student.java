ab2ffeb1bd027246360595907822d77a33e097b4
virusman1 <xirb_01@hotmail.co.th>
2019-01-22 01:39:01 +0700

    Create Student.java

---
+++ src/main/java/com/se/team00/demo/Entity/Student.java
@@ -0,0 +1,32 @@
+package com.se.team00.demo.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Column;
+import lombok.*;
+
+import javax.persistence.SequenceGenerator;
+import java.lang.annotation.Annotation;
+import javax.validation.constraints.*;
+
+@Data
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+public class Student {
+    @Id
+    @Column(unique = true)
+    @Pattern(regexp = "^[BMD]\\d{7}")
+    private String studentId;
+
+    @NotNull(message = "firstName cannot be null")
+    private  String firstName;
+
+    @NotNull(message = "lastName cannot be null")
+    private  String lastName;
+
+} 
